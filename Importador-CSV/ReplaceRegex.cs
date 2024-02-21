d047a49a480423cd750aa801a0dc4bfb89cb6c6c
Dev-Moretti <daviabelmoretti1@gmail.com>
2024-02-20 23:03:34 -0300

    Beta 1.0.4

---
+++ Importador-CSV/ReplaceRegex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Importador_CSV
+{
+    internal class ReplaceRegex
+    {
+        public ReplaceRegex()
+        {
+            
+        }
+
+        public string LimpaCaracteres(object value)
+        {
+            if (value != null)
+            {
+                string str = value.ToString().ToUpper();
+                str = Regex.Replace(str, "[ÀÁÃÂÄ]", "A");
+                str = Regex.Replace(str, "[ÈÉÊË]", "E");
+                str = Regex.Replace(str, "[ÌÍÎÏ]", "I");
+                str = Regex.Replace(str, "[ÒÓÕÔÖ]", "O");
+                str = Regex.Replace(str, "[ÙÚÛÜ]", "U");
+                str = Regex.Replace(str, "[Ç]", "C");
+                str = Regex.Replace(str, "[@#!$%&*+=]", "");
+                str = Regex.Replace(str, @"[""'']", "");
+                return str;
+            }
+
+            return "";
+        }
+
+    }
+}
